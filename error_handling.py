25bb367dd3c2fe817a9f37a9e755293525240be4
Nicole <nicbrown@udel.edu>
2021-02-17 16:09:45 -0500

    Lesson 8 ErrorHandling

---
+++ error_handling.py
@@ -0,0 +1,29 @@
+"""
+Lets clean up some errors
+"""
+
+
+# TODO: Section 1:
+
+# Run the below code to find the error, then comment out the code
+# and rewrite it with a new denominator to fix the error. Then print the 
+# type of error you got from the original problem.
+
+# number = 10 * (1/0) # FIXME
+number = 10 * 1
+print("ZeroDivisionError")
+
+# TODO: Section 1.1:
+# Again, run the code below to find what type of error you are encountering. Then
+# define 'spam' as a variable and print your error type.
+spam = 3
+equation = 4 + spam*3 # FIXME
+print("NameError")
+
+# TODO: Section 1.2:
+# Again, run the code below to find what type of error you are encountering.
+# Change the code so that our '2 + 2' addition works and print your original error type.
+
+#four = '2' + 2 # FIXME
+four = 2 + 2
+print("TypeError")
--- scrap.py
+++ scrap.py
@@ -1,14 +1,2 @@
-import random
-# You can import data or functions from files that you yourself defined. In this folder theres a
-# file called importable_stuff.py . Let's import the file and use things from it. As you learned in
-# Section 1, below is the most straight forward way to import a file.
-import importable_stuff
-
-# Then, to access the constant `DAYS_OF_WEEK` from that file,
-# you would do so with "dot notation" just like in section 1. In this import case,
-# we can find the constant by using:
-print(f"Normal dot notation: {importable_stuff.DAYS_OF_WEEK}")
-
-# You could also rename imports such as:
-import importable_stuff as stuff # Now this import can be referred to going forward as "stuff".
-print(f"Renaming as stuff: {stuff.DAYS_OF_WEEK}")
+remainder = 120 % 9
+print (remainder)
