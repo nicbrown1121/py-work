5325647c3b7dfc2e57efef068c11dcf711252bc2
Nicole <nicbrown@udel.edu>
2021-03-06 09:41:40 -0500

    Lesson 9 Indexing and Loops

--- scrap.py
+++ scrap.py
@@ -1,2 +1,11 @@
-remainder = 120 % 9
-print (remainder)
+grades = {"Chris": 65, "Deshaun": 77, "Mariah": 88, "Paula": 94}
+
+# When you use a "for loop" with a dictionary, the variable gets set to every key, but not the value.
+for student in grades:
+    print(f"{student} got a {grades[student]} on their comp sci exam.")
+    
+print("\n2nd for loop:\n")
+for student in grades:
+    grade = grades[student]
+    print(f"{student} got a {grade} on their comp sci exam.")
+
